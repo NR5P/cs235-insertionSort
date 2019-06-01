d0440db2ef8fba3d026958c15c935d32812b2b7f
Dallen R. Cluff <49734187+Cluff-Dallen@users.noreply.github.com>
2019-06-01 13:15:01 -0600

    A start on insertionSort.h
    Not sure if I'm on the right track, but a start.

--- sortInsertion.h
+++ sortInsertion.h
@@ -20,6 +20,33 @@
 template <class T>
 void sortInsertion(T array[], int num)
 {
+  // No point in continuing if num == 0.
+  if (num == 0)
+  {
+    return;
+  }
+  
+  Node <T> * pHead = new Node <T> (array[0]);
+  
+  // Loop to go through all elements inside the array.
+  for (int i = 0;i <= num; i++)
+  {
+   Node <T> *pTemp = pHead;
+    
+  }
+
+  // Loop to copy the elements from the linked-list back into the input array.
+  for (int i = 0; i < num; i++)
+  {
+    //Set each array element starting with element 0 equal to the data stored in pHead.
+    array[i] = pHead -> data;
+    
+    pHead = remove(pHead);
+  }
+  
+  
+
+  
 }
 
 #endif // INSERTION_SORT_H
